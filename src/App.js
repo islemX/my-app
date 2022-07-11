3832a1ed50dfc20a49ff4138366384a3ff067a16
islemX <islembvenamor1110@gmail.com>
2022-07-11 06:10:22 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Button, Card } from 'react-bootstrap';
+import ProfilePic from "./component/profile/ProfilePic.jpg";
+import FullName from './component/profile/FullName.js';
+import Address from './component/profile/Address.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={ProfilePic} alt='Pic'/>
+      <Card.Body>
+        <Card.Title> <FullName/> </Card.Title>
+        <Card.Text className='txt'>
+          <Address/> 
+        </Card.Text>
+        <a TARGET="_blank" href="https://www.facebook.com/islem.benamor.3"><Button class="btn btn-outline-primary" type="button" id="button-addon1"  >FB PROFILE</Button></a>
+      </Card.Body>
+    </Card>
+      
     </div>
-  );
-}
-
-export default App;
+ ); }
+ export default App;
---
+++ src/component/profile/FullName.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './fn.css';
+ const FullName = () => {
+  return ( <div>
+    <h2> ISLEM BENAMOR</h2> </div>
+  )
+}
+export default FullName;
