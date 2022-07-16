22778e7be22d5984f2af684a8c54e5877af777bf
islemX <islembvenamor1110@gmail.com>
2022-07-16 11:52:29 +0200

    appjsx

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import './App.css';
-import { Button, Card } from 'react-bootstrap';
-import ProfilePic from "./component/profile/ProfilePic.jpg";
-import FullName from './component/profile/FullName.js';
-import Address from './component/profile/Address.js';
+import { Button, Card, Col, Form, Row } from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
-       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={ProfilePic} alt='Pic'/>
-      <Card.Body>
-        <Card.Title> <FullName/> </Card.Title>
-        <Card.Text className='txt'>
-          <Address/> 
-        </Card.Text>
-        <a TARGET="_blank" href="https://www.facebook.com/islem.benamor.3"><Button class="btn btn-outline-primary" type="button" id="button-addon1"  >FB PROFILE</Button></a>
-      </Card.Body>
-    </Card>
+       <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
       
     </div>
  ); }
--- src/component/profile/FullName.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './fn.css';
- const FullName = () => {
-  return ( <div>
-    <h2> ISLEM BENAMOR</h2> </div>
-  )
-}
-export default FullName;
